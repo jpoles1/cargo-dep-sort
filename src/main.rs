a2ac003e370138745b7943fc531523b7c61eab36
Jordan Poles <jpoles1@gmail.com>
2019-04-10 18:55:26 -0400

    Cannot procede with toml crate unless on nightly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(preserve_order)]
+
 extern crate toml;
 extern crate serde;
 
@@ -12,11 +14,13 @@ pub struct CargoToml {
 }
 
 fn load_toml(filename: &str){
-    let toml_data = fs::read_to_string(filename)
+    let toml_raw = fs::read_to_string(filename)
     .expect("Something went wrong reading the file");
-    println!("Loaded in TOML data:\n{}", toml_data);
-    let toml_res : CargoToml = toml::de::from_str(&toml_data).unwrap();
-    println!("TOML parsed:\n{:?}", toml_res)
+    println!("Loaded in TOML data:\n{}", toml_raw);
+    let toml_data : toml::Value = toml::de::from_str(&toml_raw).unwrap();
+    println!("TOML parsed:\n{:?}", toml_data);
+    let toml_clean : String = toml::ser::to_string_pretty(&toml_data).unwrap();
+    println!("TOML cleaned:\n{}", toml_clean);
 }
 
 fn main() {
