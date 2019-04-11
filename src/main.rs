84d9f1f136b817996b635504c6089885f1c95873
Jordan Poles <jpoles1@gmail.com>
2019-04-10 20:27:05 -0400

    Working on manual parsing

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,51 @@
-#![feature(preserve_order)]
-
-extern crate toml;
-extern crate serde;
-
-use std::env;
+extern crate regex;
 use std::fs;
-use serde::Deserialize;
-use serde::Serialize;
+use regex::Regex;
 
-#[derive(Deserialize, Debug)]
-pub struct CargoToml {
-    dependencies: toml::value::Table
+fn load_toml(filename: &str) -> String {
+    let toml_raw = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    println!("Loaded in TOML data:\n{}", toml_raw);
+    return toml_raw
 }
 
-fn load_toml(filename: &str){
-    let toml_raw = fs::read_to_string(filename)
-    .expect("Something went wrong reading the file");
-    println!("Loaded in TOML data:\n{}", toml_raw);
-    let toml_data : toml::Value = toml::de::from_str(&toml_raw).unwrap();
-    println!("TOML parsed:\n{:?}", toml_data);
-    let toml_clean : String = toml::ser::to_string_pretty(&toml_data).unwrap();
-    println!("TOML cleaned:\n{}", toml_clean);
+fn split_toml(toml_string : String) -> (Vec<String>, Vec<String>) {
+    let toml_header_re = Regex::new(r"(\[+[\w-]+\]+)").unwrap();
+    let toml_headers : Vec<String> = toml_header_re.captures_iter(&toml_string)
+        .map(|s| s[0].to_owned()).collect();
+    let toml_parts : Vec<String> = toml_header_re.split(&toml_string)
+        .map(|s| s.to_owned()).skip(1).collect();
+    assert_eq!(toml_headers.len(), toml_parts.len());
+    return (toml_headers, toml_parts)
+}
+
+fn check_cargo_toml_sorted(toml_headers:Vec<String>, toml_parts:Vec<String>) -> bool {
+    let included_headers : Vec<&str> = vec![
+        "[dependencies]", 
+        "[dev-dependencies]",
+        "[build-dependencies]",
+        "[workspace.members]",
+        "[workspace.exclude]",
+    ];
+    for (toml_header, toml_part) in toml_headers.iter().zip(toml_parts.iter()) {
+        if included_headers.contains(&&**toml_header) {
+            println!("{} - {}", toml_header, toml_part);
+        }
+    }
+    return true
+}
+
+fn recreate_toml(toml_headers:Vec<String>, toml_parts:Vec<String>) -> String {
+    for (toml_header, toml_part) in toml_headers.iter().zip(toml_parts.iter()) {
+        println!("{} - {}", toml_header, toml_part);
+    }
+    return "test".to_string()
 }
 
 fn main() {
-    println!("Hello, world!");
-    load_toml("test_data/rustlings.toml")
+    let toml_string = load_toml("test_data/rustlings.toml");
+    let (toml_headers, toml_parts) = split_toml(toml_string);
+    println!("Headers: {:?}", toml_headers);
+    check_cargo_toml_sorted(toml_headers, toml_parts);
 }
 
