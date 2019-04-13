0eab47cb7a1491bf441795a8c7600a56b5fad90f
Jordan Poles <jpoles1@gmail.com>
2019-04-13 00:23:36 -0400

    Got sort checking functionality working with TOML crate

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,64 @@
-extern crate regex;
 extern crate clap;
+extern crate regex;
+extern crate toml;
 
 use clap::{Arg, App};
 use std::fs;
 use regex::Regex;
 
-fn load_toml(filename: &str) -> String {
-    let toml_raw = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-    return toml_raw
+//Checks if filepath points to a .toml file
+fn is_toml_filepath(filepath: &str) -> bool {
+    let toml_filepath_re = Regex::new(r"^.*\.toml$").unwrap();
+    return toml_filepath_re.is_match(filepath)
 }
 
-fn split_toml(toml_string : String) -> (Vec<String>, Vec<String>) {
-    let toml_header_re = Regex::new(r"(\[+[\w-]+\]+)").unwrap();
-    let toml_headers : Vec<String> = toml_header_re.captures_iter(&toml_string)
-        .map(|s| s[0].to_owned()).collect();
-    let toml_tables : Vec<String> = toml_header_re.split(&toml_string)
-        .map(|s| s.to_owned()).skip(1).collect();
-    assert_eq!(toml_headers.len(), toml_tables.len());
-    return (toml_headers, toml_tables)
+//Takes a file path and reads its contents in as plain text
+fn load_file_contents(filepath: &str) -> String {
+    let file_contents = fs::read_to_string(filepath)
+        .expect("ERROR: Something went wrong reading the file");
+    return file_contents
 }
 
-fn check_table_sorted(toml_table:String) -> bool {
-    let table_keys : Vec<&str> = toml_table.lines()
-        .map(|s| s.split("=").next().unwrap_or("").trim())
-        .filter(|s| s.len() > 0)
-        .collect();
+fn load_toml_file(toml_filepath: &str) -> toml::Value {
+    //Check if a valid .toml filepath
+    if !is_toml_filepath(toml_filepath) {
+        println!("WARN: detected invalid path to .toml file:\n{}", toml_filepath)
+    }   
+    //Fetch toml data
+    let toml_raw = load_file_contents(toml_filepath);
+    let toml_data = toml::de::from_str(&toml_raw);
+    if toml_data.is_err() {
+        panic!("ERROR: Failed to read improperly formatted TOML file!")
+    }
+    return toml_data.unwrap()
+}
+
+fn check_table_sorted(toml_table : &toml::value::Table) -> bool {
+    let table_keys : Vec<String> = toml_table.keys()
+        .map(|key| key.to_owned()).collect();
     let mut sorted_table_keys = table_keys.clone();
     sorted_table_keys.sort_unstable();
     return table_keys == sorted_table_keys
 }
 
-fn check_cargo_toml_sorted(toml_headers:Vec<String>, toml_tables:Vec<String>) -> Option<String> {
+fn check_cargo_toml_sorted(toml_data : toml::Value) -> Option<String> {
     let included_headers : Vec<&str> = vec![
-        "[dependencies]", 
-        "[dev-dependencies]",
-        "[build-dependencies]",
-        "[workspace.members]",
-        "[workspace.exclude]",
+        "dependencies", 
+        "dev-dependencies",
+        "build-dependencies",
+        "workspace.members",
+        "workspace.exclude",
     ];
-    for (toml_header, toml_table) in toml_headers.iter().zip(toml_tables.iter()) {
-        if included_headers.contains(&&**toml_header) {
-            println!("Checking if {} is sorted...", toml_header);
-            if !check_table_sorted(toml_table.to_string()) {
-                return Some(toml_header.to_string())
+    for table_header in included_headers.iter() {
+        if toml_data.get(table_header).is_some() {
+            let toml_table = toml_data.get(table_header).unwrap().as_table();
+            if toml_table.is_some() {
+                if !check_table_sorted(toml_table.unwrap()) {
+                    return Some(table_header.to_string())
+                }
             }
         }
+
     }
     return None
 }
@@ -54,6 +67,7 @@ fn check_cargo_toml_sorted(toml_headers:Vec<String>, toml_tables:Vec<String>) ->
 //TODO: write functions to write a properly sorted Cargo.toml file to disk
 
 fn main() {
+    //Instantiate command line args through clap
     let matches = App::new("cargo-dep-sort")
         .author("Jordan Poles <jpdev.noreply@gmail.com>")
         .about("Helps ensure sorting of Cargo.toml file dependency list")
@@ -62,15 +76,24 @@ fn main() {
             .required(true)
             .index(1))
         .get_matches();
-
-    let toml_string = load_toml(matches.value_of("INPUT").unwrap());
-    let (toml_headers, toml_tables) = split_toml(toml_string);
-    let toml_sort_result = check_cargo_toml_sorted(toml_headers, toml_tables);
+    let toml_filepath = matches.value_of("INPUT").unwrap();
+    let toml_data = load_toml_file(toml_filepath);
+    let toml_sort_result = check_cargo_toml_sorted(toml_data);
     if toml_sort_result.is_some() {
-        eprintln!("Error: found unsorted Cargo.toml table: {}", toml_sort_result.unwrap());
+        eprintln!("FAIL: found unsorted Cargo.toml table: {}", toml_sort_result.unwrap());
         std::process::exit(65);
     }
-    println!("Success: the detected Cargo.toml tables are properly sorted!");
+    println!("PASS: the detected Cargo.toml file is properly sorted!");
     std::process::exit(0);
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_toml_filepath() {
+        assert!(is_toml_filepath("/cargo.toml"));
+        assert!(!is_toml_filepath("cargo.tomls"));
+    }
+}
